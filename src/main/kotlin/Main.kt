cee40f1de68fb3b095d03318639581689c34ec10
Andrew Tieszen <pilottzn@gmail.com>
2023-09-18 18:57:27 -0600

    "evenOrOdd" function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    val number = args[0]
+    val number = if(args.isNotEmpty()) args[0].toIntOrNull() else null
+    println("$number is ${evenOrOdd(number)}.")
 }
 
-fun evenOrOdd(num: Int): String {
-
+fun evenOrOdd(num: Int?): String {
+    if (num != null) {
+        return if (num % 2 == 0) "even" else "odd"
+    }
+    return "invalid input. Please enter a valid number"
 }
